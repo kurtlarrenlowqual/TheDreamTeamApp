b1b70a1962e9d09fa0be32d06d3d89e7784663b8
LeonardXD <leonardsamaniego14@gmail.com>
2026-01-15 15:09:21 +0800

    Student Five

--- app/src/main/java/com/example/thedreamteamapp/StudentFiveScreen.kt
+++ app/src/main/java/com/example/thedreamteamapp/StudentFiveScreen.kt
@@ -1,2 +1,12 @@
 package com.example.thedreamteamapp
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StudentFiveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_two)
+    }
+}
